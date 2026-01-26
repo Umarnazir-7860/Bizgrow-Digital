4c285a10e8edb2192e88f32a19b304dfd85129a6
Umarnazir-7860 <nazirumar@7860.gmail.com>
2026-01-26 13:27:52 +0500

    Fix the fetching of blogs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true, // Is se automatic / add ho jayega
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'bizgrow-digital.co.uk',
+        hostname: 'bizgrow-digital.co.uk', // Isay add karein
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cms.bizgrow-digital.co.uk', // Subdomain backup ke liye
         pathname: '/**',
       },
     ],
